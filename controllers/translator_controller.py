01120aedbf6c4be0a2e5c2ddfd6eea466fe9f953
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-04-15 19:16:06 +0300

    edit path static demo screen

--- controllers/translator_controller.py
+++ controllers/translator_controller.py
@@ -1,12 +1,12 @@
 from models.database_model import *
-from views.translator_view import *
+from views.translator_adaptive_view import *
 
 class TranslatorController():
 	def __init__(self):
 		self.database = DatabaseTxt(CONST_NAMEFILE_DATABASE_TXT)
 		data_en = self.database.get_all_en()
 		data_ru = self.database.get_all_ru()
-		self.view = TranslatorView(data_en, data_ru)
+		self.view = TranslatorAdaptiveView(data_en, data_ru)
 
 	
 	def run(self):
